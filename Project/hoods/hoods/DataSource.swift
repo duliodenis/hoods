eadefda3acf88861e24d5b9f0b6131fd89461fbd
Andrew Carvajal <andrewcarvajal1@gmail.com>
2016-08-10 23:57:34 -0400

    added location manager singleton, minimized feed view that shows the current hood name using a hood check in the data source, and camera funcs

--- Project/hoods/hoods/DataSource.swift
+++ Project/hoods/hoods/DataSource.swift
@@ -14,24 +14,54 @@ class DataSource {
     static let sharedInstance = DataSource()
     private init() {}
     
+    var locationManager = CLLocationManager()
+    var lastHoodName: String?
+    var lastPolygonRenderer: MKPolygonRenderer?
     var calloutRepresentedObjectTitle = ""
-    var currentHoodName: String?
-    var currentPolygonRenderer: MKPolygonRenderer?
     
-    private func currentHoodFromAllHoods(currentLocation: CLLocationCoordinate2D) -> String {
+    func currentHoodName(currentLocation: CLLocationCoordinate2D) -> String {
         
-        // point to manualNYC.geojson and use SwiftyJSON
+        // if your coords are not in the last polygon...
+        if stillInTheHood(currentLocation) == false {
+            
+            // check through all hood polygons for your coords and update last hood name
+            lastHoodName = fullHoodCheck(currentLocation)
+            
+            // if full hood check failed, send notification to set scanningHoods to false and return blank string
+            if lastHoodName == "" {
+                NSNotificationCenter.defaultCenter().postNotificationName("NotInAHood", object: nil)
+                return ""
+                
+            // if full hood check succeeded, return last hood name
+            } else {
+                return lastHoodName!
+            }
+            
+        // else you have been to a hood and your coords are in the last polygon, return last hood name
+        } else if lastHoodName != nil {
+            print("You are still in \(lastHoodName!).")
+            return lastHoodName!
+            
+        // else your coords are in the last polygon, return blank string
+        } else {
+            return ""
+        }
+    }
+    
+    private func fullHoodCheck(currentLocation: CLLocationCoordinate2D) -> String {
+        
+        // convert GeoJSON to NSData
         let filePath = NSBundle.mainBundle().pathForResource("manualNYC", ofType: "geojson")!
         let data = NSData(contentsOfFile: filePath)
         do {
             let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
             
             if let hoods = json["features"] as? [[String: AnyObject]] {
+                
+                // iterate through all hoods
                 for hood in hoods {
                     
-                    // set up an empty array that will be each polygon
                     var coords = [CLLocationCoordinate2D]()
-                    
                     var currentNeighborhood = ""
                     
                     if let properties = hood["properties"] as? [String: AnyObject] {
@@ -40,6 +70,7 @@ class DataSource {
                         }
                     }
                     
+                    // add the coord pairs to the coords array
                     if let geometry = hood["geometry"] as? [String: AnyObject] {
                         if let coordinates = geometry["coordinates"] as? [[[Float]]] {
                             for array in coordinates {
@@ -49,16 +80,20 @@ class DataSource {
                                     coords.append(CLLocationCoordinate2DMake(latitude, longitude))
                                 }
                                 
+                                // create the polygon renderer from the polygon from the coords array
                                 let polygon = MKPolygon(coordinates: &coords, count: coords.count)
                                 let polygonRenderer = MKPolygonRenderer(polygon: polygon)
                                 
+                                // CLLCoordinate2D -> MKMapPoint -> check if CGPoint is inside polygon renderer's CGPath
                                 let mapPoint = MKMapPointForCoordinate(currentLocation)
                                 let cgPoint = polygonRenderer.pointForMapPoint(mapPoint)
                                 
                                 if CGPathContainsPoint(polygonRenderer.path, nil, cgPoint, true) {
                                     
-                                    currentPolygonRenderer = polygonRenderer
-                                    currentHoodName = currentNeighborhood
+                                    // update the name and polygon renderer
+                                    lastPolygonRenderer = polygonRenderer
+                                    lastHoodName = currentNeighborhood
+                                    
                                     print("You are in \(currentNeighborhood).")
                                     return currentNeighborhood
                                 } else {
@@ -73,5 +108,30 @@ class DataSource {
             print("error serializing JSON: \(error)")
         }
         return ""
+    }
+    
+    private func stillInTheHood(currentLocation: CLLocationCoordinate2D) -> Bool {
+        
+        // if gps is working
+        if locationManager.location != nil {
+            
+            // and you have been to a hood
+            if lastPolygonRenderer != nil {
+                
+                let mapPoint = MKMapPointForCoordinate(currentLocation)
+                let cgPoint = lastPolygonRenderer!.pointForMapPoint(mapPoint)
+                
+                // check if your coords are in the last polygon renderer path
+                if CGPathContainsPoint(lastPolygonRenderer!.path, nil, cgPoint, true) {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
     }
 }
